0df0badab569734f42d7087c0dc38d075bebb07b
farehiqbal <fareh.iqbal55@gmail.com>
2024-11-22 11:15:13 +0500

    minor changes

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("New reg no is" + "FA21-BCS-019");
+        System.out.println("New reg no is " + "FA21-BCS-019" + "with updated commit");
     }
 }
